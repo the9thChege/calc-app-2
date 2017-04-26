21cd55dde136e587decd969234c8acc58ce88543
Chege <chegekformal@gmail.com>
2017-04-26 18:52:42 +0300

    Updated the UI; now adds

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 var add = function(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Here, you can add two numbers. Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert("The result is " + add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
